f4cd38ec372bd58045ebda11b37e48a11d3d091c
murodjon-hub <murodjon.woosong@gmail.com>
2025-09-18 21:26:58 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require('http');
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8",(err,data) => {
+if(err){
+    console.log("ERROR:", err);
+} else {
+    user = JSON.parse(data);
+}
+});
+
 //1: Kirish code
 app.use(express.static("public"));
 app.use(express.json());
@@ -30,8 +41,14 @@ app.get("/",function(req, res){
 res.render("harid");
 });
 
+app.get('/author',(req, res) => {
+ res.render("author", {user:user});
+});
+
+
 const server = http.createServer(app);
-let PORT = 1005;
+let PORT = 3000;
 server.listen(PORT, function () {
     console.log(`The server is running successfilly on port: ${PORT} `);
 });
+
