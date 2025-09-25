9f561c0a9da6e4245fb84fa37c4a6202890160e5
murodjon-hub <murodjon.woosong@gmail.com>
2025-09-25 10:48:46 +0900

    fix: refactoring

--- app.js
+++ app.js
@@ -2,14 +2,8 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const res = require("express/lib/response")
 const app = express();
-
-
-
 //MongoDB chaqirish
-const db =require("./server").db();
-
-
-
+const { db } = require("./server");
 const fs = require("fs");
 let user;
 fs.readFile("database/user.json", "utf8",(err,data) => {
@@ -19,14 +13,11 @@ if(err){
     user = JSON.parse(data);
 }
 });
-
 //1: Kirish code
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
 //2: Session code
-
 //3: Views code
 app.set("views","views");
 app.set("view engine", "ejs");
@@ -41,17 +32,24 @@ app.set("view engine", "ejs");
 // });
 app.post("/create-item", (req, res) =>{
 console.log(req.body);
-res.json({test:"success"})
+res.end("succes")
+// res.json({test:"success"})
 });
-
 app.get("/",function(req, res){
+    db.collection("plans").find().toArray(function(err,data){
+        if(err){
+            console.log(err);
+            res.end("something went wrong");
+        }else {
+                console.log(data);
+                res.render("reja");
+            }
+    }); 
 res.render("reja");
 });
-
 app.get('/author',(req, res) => {
  res.render("author", {user:user});
 });
 
-module.exports = app;
-
+module.exports = db;
 
--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-const http = require('http');
-const mongodb = require ("mongodb");
-
-
+const http = require("http");
+const mongodb = require("mongodb"); 
 let db;
-const connectionString = 
-"mongodb+srv://murodjonwoosong_db_user:pSBy0aIiJL5kSmYX@cluster0.x2lbima.mongodb.net/"
-mongodb.connect(connectionString, {
-    useNewUrlParser: true, 
+const connectionString =
+  "mongodb+srv://murodjonwoosong_db_user:pSBy0aIiJL5kSmYX@cluster0.x2lbima.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
     useUnifiedTopology: true,
-}, 
-(err, client) =>{
-if(err) console.log("ERROR on connection MongoDB");
-else {
-    console.log("MongoDB connection succseed");
-    module.exports = client;
-    
-    const app = require("./app");
-    const server = http.createServer(app);
-    let PORT = 3000;
-    server.listen(PORT, function () {
-    console.log(`The server is running successfilly on port: ${PORT}, http://localhost:${PORT} `
+  },
+  (err, client) => {
+    if (err) {
+      console.log("ERROR on connection MongoDB");
+    } else {
+      db = client.db(); // db obyektini saqlash
+      console.log("MongoDB connection succeed");
 
-    );
-});
-}
-}
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
+  }
 );
-
