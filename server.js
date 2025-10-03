dad20e247d956d66ba87e6c5b6cf6e99b2d8028e
murodjon-hub <murodjon.woosong@gmail.com>
2025-10-03 11:34:45 +0900

    feat: new

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongodb.connect(
       module.exports = client;
       const app = require("./app");
       const server = http.createServer(app);
-      let PORT = 3000;
+      let PORT = 4009;
 
       server.listen(PORT, function () {
         console.log(
